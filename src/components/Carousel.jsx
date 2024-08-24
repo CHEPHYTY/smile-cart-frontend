6f77f177cbb19636863eb8ac01ead740fa20130e
CHEPHYTY <soumyahembra123@gmail.com>
2024-08-24 18:55:40 +0530

    Integrated Ref to the Carousel component

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 import classNames from "classnames";
 import { Left, Right } from "neetoicons";
@@ -6,11 +6,19 @@ import { Button } from "neetoui";
 
 const Carousel = ({ imageUrls, title }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
+
+  const timerRef = useRef(null);
+
   const handleNext = () => {
     // const nextIndex = (currentIndex + 1) % imageUrls.length;
     // setCurrentIndex(nextIndex);
     // console.log(currentIndex);
     setCurrentIndex(prevIndex => (prevIndex + 1) % imageUrls.length);
+  };
+
+  const resetTimer = () => {
+    clearInterval(timerRef.current);
+    timerRef.current = setInterval(handleNext, 3000);
   };
 
   const handlePrevious = () => {
@@ -19,12 +27,13 @@ const Carousel = ({ imageUrls, title }) => {
     // setCurrentIndex(previousIndex);
     // console.log(currentIndex);
     setCurrentIndex(prevIndex => (prevIndex - 1) % imageUrls.length);
+    resetTimer();
   };
 
   useEffect(() => {
-    const interval = setInterval(handleNext, 3000);
+    timerRef.current = setInterval(handleNext, 3000);
 
-    return () => clearInterval(interval);
+    return () => clearInterval(timerRef.current);
   }, []);
 
   return (
@@ -46,7 +55,10 @@ const Carousel = ({ imageUrls, title }) => {
           className="shrink-0 focus-within:ring-0 hover:bg-transparent"
           icon={Right}
           style="text"
-          onClick={handleNext}
+          onClick={() => {
+            handleNext();
+            resetTimer();
+          }}
         />
       </div>
       <div className="flex space-x-1">
@@ -68,7 +80,10 @@ const Carousel = ({ imageUrls, title }) => {
                 "neeto-ui-border-black neeto-ui-rounded-full h-3 w-3 cursor-pointer border",
                 { "neeto-ui-bg-black": index === currentIndex }
               )}
-              onClick={() => setCurrentIndex(index)}
+              onClick={() => {
+                setCurrentIndex(index);
+                resetTimer();
+              }}
             />
           );
         })}
