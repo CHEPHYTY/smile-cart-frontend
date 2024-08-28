7e81edadae36aed1645dea6a4725aa0a26184932
chephyty <soumyahembra123@gmail.com>
2024-08-29 00:22:00 +0530

    Added product listing page

--- src/components/Product/Carousel.jsx
+++ src/components/Product/Carousel.jsx
@@ -1,3 +1,4 @@
+// import { useState, useEffect } from "react";
 import { useState, useEffect, useRef } from "react";
 
 import classNames from "classnames";
@@ -9,30 +10,27 @@ const Carousel = ({ imageUrls, title }) => {
 
   const timerRef = useRef(null);
 
-  const handleNext = () => {
-    // const nextIndex = (currentIndex + 1) % imageUrls.length;
-    // setCurrentIndex(nextIndex);
-    // console.log(currentIndex);
+  const handleNext = () =>
     setCurrentIndex(prevIndex => (prevIndex + 1) % imageUrls.length);
-  };
 
+  // const handlePrevious = () =>
   const resetTimer = () => {
     clearInterval(timerRef.current);
     timerRef.current = setInterval(handleNext, 3000);
   };
 
   const handlePrevious = () => {
-    // const previousIndex =
-    //   (currentIndex - 1 + imageUrls.length) % imageUrls.length;
-    // setCurrentIndex(previousIndex);
-    // console.log(currentIndex);
-    setCurrentIndex(prevIndex => (prevIndex - 1) % imageUrls.length);
+    setCurrentIndex(
+      prevIndex => (prevIndex - 1 + imageUrls.length) % imageUrls.length
+    );
     resetTimer();
   };
 
   useEffect(() => {
+    // const interval = setInterval(handleNext, 3000);
     timerRef.current = setInterval(handleNext, 3000);
 
+    // return () => clearInterval(interval);
     return () => clearInterval(timerRef.current);
   }, []);
 
@@ -40,21 +38,21 @@ const Carousel = ({ imageUrls, title }) => {
     <div className="flex flex-col items-center">
       <div className="flex items-center">
         <Button
+          alt={title}
           className="shrink-0 focus-within:ring-0 hover:bg-transparent"
           icon={Left}
           style="text"
           onClick={handlePrevious}
         />
         <img
-          alt={title}
           className="max-w-56 h-56 max-h-56 w-56"
           src={imageUrls[currentIndex]}
         />
         <Button
-          alt={title}
           className="shrink-0 focus-within:ring-0 hover:bg-transparent"
           icon={Right}
           style="text"
+          // onClick={handleNext}
           onClick={() => {
             handleNext();
             resetTimer();
@@ -62,34 +60,22 @@ const Carousel = ({ imageUrls, title }) => {
         />
       </div>
       <div className="flex space-x-1">
-        {imageUrls.map((_, index) => {
-          {
-            /* const defaultClasses =
-            "neeto-ui-border-black neeto-ui-rounded-full h-3 w-3 cursor-pointer border";
-
-          const dotClassNames =
-            index === currentIndex
-              ? defaultClasses.concat(" neeto-ui-bg-black")
-              : defaultClasses; */
-          }
-
-          return (
-            <span
-              key={index}
-              className={classNames(
-                "neeto-ui-border-black neeto-ui-rounded-full h-3 w-3 cursor-pointer border",
-                { "neeto-ui-bg-black": index === currentIndex }
-              )}
-              onClick={() => {
-                setCurrentIndex(index);
-                resetTimer();
-              }}
-            />
-          );
-        })}
+        {imageUrls.map((_, index) => (
+          <span
+            key={index}
+            className={classNames(
+              "neeto-ui-border-black neeto-ui-rounded-full h-3 w-3 cursor-pointer border",
+              { "neeto-ui-bg-black": index === currentIndex }
+            )}
+            // onClick={() => setCurrentIndex(index)}
+            onClick={() => {
+              setCurrentIndex(index);
+              resetTimer();
+            }}
+          />
+        ))}
       </div>
     </div>
   );
 };
-
 export default Carousel;
